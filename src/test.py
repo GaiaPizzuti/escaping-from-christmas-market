74e2904f9939cc17089e87e32879f90c9bb290dc
pariamelle <mariaamalia.pelle@studenti.unitn.it>
2025-01-15 12:47:53 +0100

    added code for boidguard

--- src/test.py
+++ src/test.py
@@ -4,6 +4,7 @@ import random
 import pygame as pg
 
 from boid import Boid
+from boidguard import BoidGuard
 
 sys.path.append(".")
 
@@ -17,8 +18,9 @@ OBJ_COLOR = config["color"]["target"]
 
 MAP = config["image"]["path"]
 BOIDS = config["people"]["num"]
+BOIDGUARDS = config["security"]["nums"]
 
-def run(WIDTH, HEIGHT, BOIDS, alignment, cohesion, separation):
+def run(WIDTH, HEIGHT, BOIDS, BOIDGUARDS, alignment, cohesion, separation):
     pg.init()
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     image = pg.image.load(MAP).convert()
@@ -30,6 +32,7 @@ def run(WIDTH, HEIGHT, BOIDS, alignment, cohesion, separation):
 
     # create boids
     boids = [Boid(WIDTH, HEIGHT) for _ in range(BOIDS)]
+    boidguards = [BoidGuard(WIDTH, HEIGHT) for _ in range(BOIDGUARDS)]
 
     running = True
     while running:
@@ -44,10 +47,15 @@ def run(WIDTH, HEIGHT, BOIDS, alignment, cohesion, separation):
             boid.draw(screen)
             boid.update(boids, alignment, cohesion, separation)
 
+
+        for boidg in boidguards:
+            boidg.draw(screen)
+            boidg.update(boidguards, alignment, cohesion, separation)
+
         # update the screen
         pg.display.flip()
         clock.tick(60)
     pg.quit()
 
 if __name__ == "__main__":
-    run(WIDTH, HEIGHT, BOIDS, 0.5, 0.5, 0.5)
+    run(WIDTH, HEIGHT, BOIDS, BOIDGUARDS, 0.5, 0.5, 0.5)
